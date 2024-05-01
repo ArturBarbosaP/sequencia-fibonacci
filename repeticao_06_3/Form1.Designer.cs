e1fe32106ea9be673c7b5f90c805220d1a871ed1
Artur Barbosa <arturbarbosap@gmail.com>
2024-05-01 12:02:54 -0300

    Adicionar arquivos de projeto.

---
+++ repeticao_06_3/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿
+namespace repeticao_06_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ltx_imprimir = new System.Windows.Forms.ListBox();
+            this.btn_calcular = new System.Windows.Forms.Button();
+            this.lbl_num = new System.Windows.Forms.Label();
+            this.txt_num = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // ltx_imprimir
+            // 
+            this.ltx_imprimir.FormattingEnabled = true;
+            this.ltx_imprimir.Location = new System.Drawing.Point(11, 38);
+            this.ltx_imprimir.Name = "ltx_imprimir";
+            this.ltx_imprimir.Size = new System.Drawing.Size(221, 342);
+            this.ltx_imprimir.TabIndex = 0;
+            // 
+            // btn_calcular
+            // 
+            this.btn_calcular.Location = new System.Drawing.Point(11, 397);
+            this.btn_calcular.Name = "btn_calcular";
+            this.btn_calcular.Size = new System.Drawing.Size(221, 30);
+            this.btn_calcular.TabIndex = 1;
+            this.btn_calcular.Text = "Calcular";
+            this.btn_calcular.UseVisualStyleBackColor = true;
+            this.btn_calcular.Click += new System.EventHandler(this.btn_calcular_Click);
+            // 
+            // lbl_num
+            // 
+            this.lbl_num.AutoSize = true;
+            this.lbl_num.Location = new System.Drawing.Point(12, 15);
+            this.lbl_num.Name = "lbl_num";
+            this.lbl_num.Size = new System.Drawing.Size(92, 13);
+            this.lbl_num.TabIndex = 2;
+            this.lbl_num.Text = "Digite um número:";
+            // 
+            // txt_num
+            // 
+            this.txt_num.Location = new System.Drawing.Point(132, 12);
+            this.txt_num.Name = "txt_num";
+            this.txt_num.Size = new System.Drawing.Size(100, 20);
+            this.txt_num.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(242, 437);
+            this.Controls.Add(this.txt_num);
+            this.Controls.Add(this.lbl_num);
+            this.Controls.Add(this.btn_calcular);
+            this.Controls.Add(this.ltx_imprimir);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Fibonacci";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox ltx_imprimir;
+        private System.Windows.Forms.Button btn_calcular;
+        private System.Windows.Forms.Label lbl_num;
+        private System.Windows.Forms.TextBox txt_num;
+    }
+}
+
---
+++ repeticao_06_3/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace repeticao_06_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_calcular_Click(object sender, EventArgs e)
+        {
+            int cont = 1, n, num1 = 0, num2= 1, num3 = 0;
+            n = int.Parse(txt_num.Text);
+
+            ltx_imprimir.Items.Clear();
+
+            while (cont <= n)
+            {
+                ltx_imprimir.Items.Add(cont + "  -  " + num3);
+                cont++;
+                num1 = num2;
+                num2 = num3;
+                num3 = num1 + num2;
+            }
+        }
+    }
+}
